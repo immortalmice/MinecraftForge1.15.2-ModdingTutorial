7cf6022295006c516394a161a0c0f0d285494e1c
immortalmice <immortalmice@users.noreply.github.com>
2020-04-29 20:32:59 +0800

    Finish Chapter1 Section 1,2
    Finish Chapter1 Section 1,2

---
+++ Code/src/main/java/com/immortalmice/modtutorialim/ModTutorialIM.java
@@ -0,0 +1,14 @@
+package com.immortalmice.modtutorialim;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ModTutorialIM.MODID)
+public class ModTutorialIM{
+	public static final String MODID = "modtutorialim";
+    public static final String NAME = "Immortalmice's Mod Tutorial";
+    public static final String VERSION = "1.15.2-0.1.0";
+
+    public ModTutorialIM(){
+
+    }
+}
