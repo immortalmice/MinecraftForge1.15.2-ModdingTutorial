47b43f67637d9ca958ff8543dff871f3f67b3fbe
immortalmice <immortalmice@users.noreply.github.com>
2020-04-30 01:48:33 +0800

    Fix Bugs
    Fix versioning error
    Fix group error

--- Code/src/main/java/com/github/immortalmice/modtutorialim/ModTutorialIM.java
+++ Code/src/main/java/com/github/immortalmice/modtutorialim/ModTutorialIM.java
@@ -6,7 +6,6 @@ import net.minecraftforge.fml.common.Mod;
 public class ModTutorialIM{
 	public static final String MODID = "modtutorialim";
     public static final String NAME = "Immortalmice's Mod Tutorial";
-    public static final String VERSION = "1.15.2-0.1.0";
 
     public ModTutorialIM(){
 
